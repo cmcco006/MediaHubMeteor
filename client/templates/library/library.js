e4451577808a575fe649957c108172dc0e452ae1
cmcco006 <corey@ucrwpa1-7-10-25-24-88.wnet.ucr.edu>
2016-02-13 18:03:31 -0800

    Video player

---
+++ client/templates/library/library.js
@@ -0,0 +1,38 @@
+onReady = function (event) {
+  event.target.playVideo();
+}
+
+onStateChange = function (event) {
+  if(player.getPlayerState() == 0){
+    // currentIdplaying++;
+    player.loadVideoById(
+      'lRlmM88zzbY',
+      0,
+      'highres' //getAvailableQualityLevels() *****
+    );
+  }
+}
+
+
+onYouTubeIframeAPIReady = function () {
+  // New Video Player, the first argument is the id of the div.
+  // Make sure it's a global variable.
+  player = new YT.Player("youtube-main-player", {
+
+    height: "400",
+    width: "600",
+
+    // videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
+    videoId: "LdH1hSWGFGU",
+
+    // Events like ready, state change,
+    events: {
+      onReady: onReady,
+      onStateChange: onStateChange
+    }
+  });
+};
+
+Template.library.rendered = function(){
+  YT.load();
+}
